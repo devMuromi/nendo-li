91d7233a98d026ebea1f4bb4f8d96958824356c8
devMuromi <devmuromi@gmail.com>
2022-08-24 13:47:48 +0900

    model updated

--- nendoroid/admin.py
+++ nendoroid/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Nendoroid
+from .models import Series
+from .models import Manufacturer
+from .models import Sculptor
+
+admin.site.register(Nendoroid)
+admin.site.register(Series)
+admin.site.register(Manufacturer)
+admin.site.register(Sculptor)
--- nendoroid/models.py
+++ nendoroid/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+class Series(models.Model):
+    name_ko = models.CharField(max_length=64, blank=True)
+    name_en = models.CharField(max_length=64, blank=True)
+    name_ja = models.CharField(max_length=64, blank=True)
+
+    def __str__(self):
+        return self.name_ko
+
+class Manufacturer(models.Model):
+    name_ko = models.CharField(max_length=64, blank=True)
+    name_en = models.CharField(max_length=64, blank=True)
+    name_ja = models.CharField(max_length=64, blank=True)
+
+    def __str__(self):
+        return self.name_ko
+
+class Sculptor(models.Model):
+    name_ko = models.CharField(max_length=64, blank=True)
+    name_en = models.CharField(max_length=64, blank=True)
+    name_ja = models.CharField(max_length=64, blank=True)
+
+    def __str__(self):
+        return self.name_ko
+
+class Nendoroid(models.Model):
+    number = models.CharField(max_length=10, unique=True)
+    name_ko = models.CharField(max_length=64, blank=True)
+    name_en = models.CharField(max_length=64, blank=True)
+    name_ja = models.CharField(max_length=64, blank=True)
+    series = models.ForeignKey(Series, on_delete=models.CASCADE, null=True)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, null=True)
+    sculptor = models.ManyToManyField(Sculptor, related_name='nendoroid', null=True)
+    gsc_number = models.IntegerField(null=True)
+    release_date = models.JSONField(blank=True)
+    # image = models.ImageField(null=True)
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other')
+    ]
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+
+    def __str__(self):
+        return self.number + ' ' + self.name_ko
