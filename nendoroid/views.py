094d0dfe6090428608e108139faf7c5a7533b4a9
devMuromi <devmuromi@gmail.com>
2022-10-31 19:28:20 +0900

    change Nendoroid to genrics

--- nendoroid/views.py
+++ nendoroid/views.py
@@ -7,66 +7,51 @@ from nendoroid.serializers import NendoroidSerializer ,SeriesSerializer, Manufac
 from rest_framework.decorators import api_view
 from rest_framework import generics
 
-@api_view(['GET', 'POST'])
-def nendoroid_list(request, format=None):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == 'GET':
-        nendoroids = Nendoroid.objects.all()
-        serializer = NendoroidSerializer(nendoroids, many=True)
-        return Response(serializer.data)
+class NendoroidList(generics.ListCreateAPIView)
+    queryset = Nendoroid.objects.all()
+    serializer_class = NendoroidSerializer
 
-    elif request.method == 'POST':
-        serializer = NendoroidSerializer(data=request.data)            
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+class NendoroidDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Nendoroid.objects.all()
+    serializer_class = NendoroidSerializer
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def nendoroid_detail(request, pk, format=None):
-    try:
-        nendoroid = Nendoroid.objects.get(pk=pk)
-    except Nendoroid.DoesNotExist:
-        return Response(status=404)
+# @api_view(['GET', 'POST'])
+# def nendoroid_list(request, format=None):
+#     if request.method == 'GET':
+#         nendoroids = Nendoroid.objects.all()
+#         serializer = NendoroidSerializer(nendoroids, many=True)
+#         return Response(serializer.data)
 
-    if request.method == 'GET':
-        serializer = NendoroidSerializer(nendoroid)
-        return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = NendoroidSerializer(data=request.data)            
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors, status=400)
 
-    elif request.method == 'PUT':
-        serializer = NendoroidSerializer(nendoroid, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def nendoroid_detail(request, pk, format=None):
+#     try:
+#         nendoroid = Nendoroid.objects.get(pk=pk)
+#     except Nendoroid.DoesNotExist:
+#         return Response(status=404)
 
-            return Response(serializer.data)
-        return Response(serializer.errors, status=400)
+#     if request.method == 'GET':
+#         serializer = NendoroidSerializer(nendoroid)
+#         return Response(serializer.data)
 
-    elif request.method == 'DELETE':
-        nendoroid.delete()
-        return Response(status=204)
+#     elif request.method == 'PUT':
+#         serializer = NendoroidSerializer(nendoroid, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
 
-# from django.contrib.auth.models import User, Group
-# from rest_framework import viewsets
-# from rest_framework import permissions
-# from nendoroid.serializers import UserSerializer, GroupSerializer
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=400)
 
-# class UserViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows users to be viewed or edited.
-#     """
-#     queryset = User.objects.all().order_by('-date_joined')
-#     serializer_class = UserSerializer
-#     permission_classes = [permissions.IsAuthenticated]
+#     elif request.method == 'DELETE':
+#         nendoroid.delete()
+#         return Response(status=204)
 
-# class GroupViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows groups to be viewed or edited.
-#     """
-#     queryset = Group.objects.all()
-#     serializer_class = GroupSerializer
-#     permission_classes = [permissions.IsAuthenticated]
 
 # Series
 class SeriesList(generics.ListCreateAPIView):
